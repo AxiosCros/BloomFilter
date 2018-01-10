2b90871313197b4222e3cd497e5c07b8b7049c4c
matyhtf <mikan.tenny@gmail.com>
2018-01-10 13:17:17 +0800

    rename

---
+++ BloomFilter.cpp
@@ -0,0 +1,160 @@
+#include "phpx.h"
+
+extern "C"
+{
+#include <php_swoole.h>
+extern void MurmurHash3_x64_128(const void * key, const int len, const uint32_t seed, void *out);
+extern void SpookyHash128(const void *key, size_t len, uint64_t seed1, uint64_t seed2, uint64_t *hash1,
+        uint64_t *hash2);
+}
+
+#include <iostream>
+
+using namespace php;
+using namespace std;
+
+#define DEFAULT_QUEUE_SIZE 64
+
+struct BloomFilterObject
+{
+    size_t capacity;
+    char *array;
+    uint32_t k_num;
+    uint64_t bit_num;
+    uint64_t *hashes;
+};
+
+#define RESOURCE_NAME  "BloomFilterResource"
+#define PROPERTY_NAME  "ptr"
+
+static void compute_hashes(uint32_t k_num, char *key, size_t len, uint64_t *hashes)
+{
+    uint64_t out[2];
+    MurmurHash3_x64_128(key, len, 0, out);
+
+    hashes[0] = out[0];
+    hashes[1] = out[1];
+
+    uint64_t *hash1 = out;
+    uint64_t *hash2 = hash1 + 1;
+    SpookyHash128(key, len, 0, 0, hash1, hash2);
+
+    hashes[2] = out[0];
+    hashes[3] = out[1];
+
+    for (uint32_t i = 4; i < k_num; i++)
+    {
+        hashes[i] = hashes[1] + ((i * hashes[3]) % 18446744073709551557U);
+    }
+}
+
+static void BloomFilterResDtor(zend_resource *res)
+{
+    BloomFilterObject *bf = static_cast<BloomFilterObject *>(res->ptr);
+    efree(bf->hashes);
+    sw_shm_free(bf);
+}
+
+static PHPX_METHOD(BloomFilter, __construct)
+{
+    long capacity = args[0].toInt();
+    if (capacity <= 0)
+    {
+        capacity = 65536;
+    }
+
+    uint32_t k_num = 2;
+    if (args.exists(1))
+    {
+        k_num = (uint32_t) args[1].toInt();
+    }
+
+    BloomFilterObject *bf = (BloomFilterObject *) sw_shm_malloc(sizeof(BloomFilterObject) + capacity);
+    bf->capacity = capacity;
+    bf->array = (char *) (bf + 1);
+    bzero(bf->array, bf->capacity);
+
+    bf->hashes = (uint64_t*) ecalloc(k_num, sizeof(uint64_t));
+    bf->bit_num = bf->capacity * 8;
+    bf->k_num = k_num;
+
+    _this.oSet(PROPERTY_NAME, RESOURCE_NAME, bf);
+}
+
+PHPX_METHOD(BloomFilter, has)
+{
+    BloomFilterObject *bf = _this.oGet<BloomFilterObject>(PROPERTY_NAME, RESOURCE_NAME);
+    auto key = args[0];
+    compute_hashes(bf->k_num, key.toCString(), key.length(), bf->hashes);
+
+    uint32_t i;
+    uint32_t n;
+    bool miss;
+
+    for (i = 0; i < bf->k_num; i++)
+    {
+        n = bf->hashes[i] % bf->bit_num;
+        miss = !(bf->array[n / 8] & (1 << (n % 8)));
+        if (miss)
+        {
+            retval = false;
+            return;
+        }
+    }
+
+    retval = true;
+}
+
+PHPX_METHOD(BloomFilter, add)
+{
+    BloomFilterObject *bf = _this.oGet<BloomFilterObject>(PROPERTY_NAME, RESOURCE_NAME);
+    auto key = args[0];
+    compute_hashes(bf->k_num, key.toCString(), key.length(), bf->hashes);
+
+    uint32_t i;
+    uint32_t n;
+
+    for (i = 0; i < bf->k_num; i++)
+    {
+        n = bf->hashes[i] % bf->bit_num;
+        bf->array[n / 8] |= (1 << (n % 8));
+    }
+}
+
+PHPX_EXTENSION()
+{
+    Extension *extension = new Extension("BloomFilter", "0.0.1");
+
+    extension->onStart = [extension]() noexcept
+    {
+        extension->registerConstant("QUEUE_VERSION", 1001);
+
+        Class *c = new Class("BloomFilter");
+        c->addMethod(PHPX_ME(BloomFilter, __construct), CONSTRUCT);
+        c->addMethod(PHPX_ME(BloomFilter, add));
+        c->addMethod(PHPX_ME(BloomFilter, has));
+
+        extension->registerClass(c);
+        extension->registerResource(RESOURCE_NAME, BloomFilterResDtor);
+    };
+
+    //extension->onShutdown = [extension]() noexcept {
+    //};
+
+    //extension->onBeforeRequest = [extension]() noexcept {
+    //    cout << extension->name << "beforeRequest" << endl;
+    //};
+
+    //extension->onAfterRequest = [extension]() noexcept {
+    //    cout << extension->name << "afterRequest" << endl;
+    //};
+
+    extension->info({"BloomFilter support", "enabled"},
+                    {
+                        {"author", "Tianfeng Han"},
+                        {"version", extension->version},
+                        {"date", "2018-01-10"},
+                    });
+
+    return extension;
+}
--- tests/main.php
+++ tests/main.php
@@ -1,21 +1,16 @@
 <?php
-$b = new bitmap(1024);
-echo "bit 1\n";
-$b->set(1, 0);
-var_dump($b->get(1));
+$b = new BloomFilter(1024);
+echo "key hello\n";
 
-echo "bit 1\n";
-$b->set(1, 1);
-var_dump($b->get(1));
+$b->add("hello");
+$b->add("world");
+$b->add("rango");
 
-echo "bit 7\n";
-$b->set(7, 1);
-var_dump($b->get(7));
-var_dump($b->get(0));
-
-echo "bit 8\n";
-$b->set(8, 1);
-var_dump($b->get(9));
-var_dump($b->get(8));
-var_dump($b->get(7));
-var_dump($b->get(0));
+var_dump($b->has("test"));
+var_dump($b->has("hello"));
+var_dump($b->has("world"));
+var_dump($b->has(" world "));
+var_dump($b->has("rango"));
+var_dump($b->has("xxxxxxxxxxxxxxxxxxxxxxx"));
+var_dump($b->has("rango12"));
+var_dump($b->has("me"));
